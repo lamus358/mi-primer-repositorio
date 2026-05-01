30b373e97f18f21dddc7726b94e25d5dac019226
lamus358 <lamus0201@gmail.com>
2026-05-01 13:30:11 -0500

    primer ejercicio con python
    empezando a trabajar con Python aprendiendo sobre comentarios variables función print, función input, función type y concatenación

---
+++ proyectos/variables.py
@@ -0,0 +1,33 @@
+#Esto es un comentario de una sola linea
+"""esto es un comentario
+de varias lineas"""
+
+#inicializando variables
+nombre="Jaime Aalejandro Lamus Gonzalez" 
+Edad=14
+estado=true
+nota=5.0
+
+#mostrar el contenido de las variables print()
+print(nombre)
+print(edad)
+print(estado)
+print(nota)
+
+#que tipo de dato contiene cada variable.
+print(type(nombre))
+print(type(edad))
+print(type(estado))
+print(type(nota))
+
+#vamos a utilizar la funcion input para recpjer datos por medio del teclado
+nombre=input("¿como te llamas? ")
+edad=input("¿que edad tienes? ")
+estado=input("¿en que estado te encuentras? ")
+nota=input("¿cual es tu nota? ")
+
+#para visualizar que guardamos en las variables anteriores
+print("hola,",nombre,"un gusto conocerte")
+print("tu edad es:",edad)
+print("tu estado es:",estado)
+print("tu nota es:",nota)
